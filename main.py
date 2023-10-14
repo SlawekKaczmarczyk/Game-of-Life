5d7435fb6b61958ec98385e360388129273d8c0f
SlawekKaczmarczyk <slawomir.kaczmarczyk1998@gmail.com>
2023-10-14 16:09:40 +0200

    Draw grid function added

--- main.py
+++ main.py
@@ -2,12 +2,32 @@ import pygame
 
 pygame.init()
 
+BLACK = (0,0,0)
+GREY = (127,127,127)
+ORANGE = (255,165,0)
+
+WIDTH, HEIGHT = 800, 800
+TILE_SIZE = 20
+GRID_WIDTH = WIDTH // TILE_SIZE
+GRID_HEIGHT = HEIGHT // TILE_SIZE
 FPS = 60
+
+screen = pygame.display.set_mode((WIDTH,HEIGHT))
 
 clock = pygame.time.Clock()
 
+def draw_grid(positions):
+    for row in range(GRID_HEIGHT):
+        pygame.draw.line(screen, BLACK,(0, row * TILE_SIZE),(WIDTH, row * TILE_SIZE))
+    
+    for column in range(GRID_HEIGHT):
+        pygame.draw.line(screen, BLACK,(column * TILE_SIZE, 0),(column * TILE_SIZE, HEIGHT))
+
+
 def main():
     running = True
+
+    positions =set()
 
     while running:
         clock.tick(FPS)
@@ -15,7 +35,11 @@ def main():
         for event in pygame.event.get():
             if event. type == pygame.QUIT:
                 running = False
-    
+
+        screen.fill(GREY)            
+        draw_grid(positions)
+        pygame.display.update()
+
     pygame.quit()
 
 if __name__ == "__main__":
